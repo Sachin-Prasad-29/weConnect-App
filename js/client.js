794ae1a69a24dfe442596a19cec285f5b3451b69
Sachin <skpkorba9009@gmail.com>
2022-07-14 00:13:30 +0530

    Added all the requirements ready to deploy

--- js/client.js
+++ js/client.js
@@ -1,25 +1,49 @@
 const socket = io("http://localhost:8000");
 
+//get Dom elements in respective JS variable
 const form = document.getElementById("send-form");
 const messageInput = document.getElementById("messageInput");
 const messageContainer = document.querySelector(".message-box");
+//tone which will fire on chat
+let audio = new Audio('/resources/ting.mp3');
 
+// Function which will append event info to the container
 const append = (message, position) => {
   const messageElement = document.createElement("div");
   messageElement.innerHTML = message;
   messageElement.classList.add("message");
   messageElement.classList.add(position);
   messageContainer.append(messageElement);
+  if(position !== 'right')
+       audio.play();
+
+  
 };
 
+// Asking user for the name
 const names = prompt("Enter your name to Join the Chat");
 socket.emit("new-user-joined", names);
 
+
+// IF a new user join recive the event 
 socket.on("user-joined", (names) => {
-  append(`${names} joined the chat`, 'center');
+  append(`<span>${names} joined the chat</span>`, "center");
+});
+
+//if server sends the me
+socket.on("receive", (data) => {
+  append(`${data.names}: ${data.message}`, "left");
+});
+
+socket.on("left", (names) => {
+  append(`${names} left the char`, "center");
 });
 
 
-socket.on("receive", (data) => {
-    append(`${data.message}: ${data.user}`, 'left');
-  });
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const message = messageInput.value;
+  append(`You: ${message}`, "right");
+  socket.emit("send", message);
+  messageInput.value = "";
+});
--- weConnect-Server/index.js
+++ weConnect-Server/index.js
@@ -5,13 +5,22 @@ const io = require("socket.io")(8000);
 const users = {};
 
 io.on("connection", (socket) => {
-  socket.on("new-user-joined", (names) => {
-    console.log("New user ", names);
-    users[socket.id] = names;
-    socket.broadcast.emit("user-joined", names);
+  // IF any new user joins , lets other users connected to the server know
+  socket.on("new-user-joined", (name) => {
+   // console.log("New user ", names);
+    users[socket.id] = name;
+    socket.broadcast.emit("user-joined", name);
   });
 
+  //If someone sends a message , broadcast it to other people
   socket.on("send", (message) => {
-    socket.broadcast.emit("recieve", { message: message, names: users[socket.id] });
+    socket.broadcast.emit("receive", { message: message, names: users[socket.id] });
   });
+
+  //If someone leaves the chat let other know about it
+  socket.on("disconnect", (message) => {
+    socket.broadcast.emit("left", users[socket.id]);
+    delete users[socket.id];
+  });
+
 });
